e3ffb5b9eef85422d2c07e88f2d58fd05340f9fc
weeger <romain.weeger@gmail.com>
2025-11-07 15:52:26 +0100

    Various

---
+++ examples/various/simple_event_example.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from wexample_helpers.classes.example.example import Example
+
+
+class SimpleEventExample(Example):
+    def execute(self) -> None:
+        print('EXAMPLE')
